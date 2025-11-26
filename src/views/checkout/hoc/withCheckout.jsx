ce6e8021f8a54f255e993dbd8c27b7c5f1df7f2c
Jean-Ludovic <144408053+Jean-Ludovic@users.noreply.github.com>
2025-11-26 07:36:01 -0500

    blidage checkout

--- src/views/checkout/hoc/withCheckout.jsx
+++ src/views/checkout/hoc/withCheckout.jsx
@@ -5,36 +5,49 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { Redirect, withRouter } from 'react-router-dom';
 
-const withCheckout = (Component) => withRouter((props) => {
-  const state = useSelector((store) => ({
-    isAuth: !!store.auth.id && !!store.auth.role,
-    basket: store.basket,
-    shipping: store.checkout.shipping,
-    payment: store.checkout.payment,
-    profile: store.profile
-  }));
+const withCheckout = (Component) =>
+  withRouter((props) => {
+    const state = useSelector((store) => ({
+      isAuth: !!store.auth.id && !!store.auth.role,
+      basket: store.basket || [],
+      shipping: store.checkout?.shipping || {},
+      payment: store.checkout?.payment || {},
+      profile: store.profile || {}
+    }));
 
-  const shippingFee = state.shipping.isInternational ? 50 : 0;
-  const subtotal = calculateTotal(state.basket.map((product) => product.price * product.quantity));
+    // sécuriser le shipping
+    const isInternational = !!state.shipping?.isInternational;
+    const shippingFee = isInternational ? 50 : 0;
 
-  if (!state.isAuth) {
-    return <Redirect to={SIGNIN} />;
-  } if (state.basket.length === 0) {
-    return <Redirect to="/" />;
-  } if (state.isAuth && state.basket.length !== 0) {
+    // sécuriser le panier
+    const basketArray = Array.isArray(state.basket) ? state.basket : [];
+    const subtotal = calculateTotal(
+      basketArray.map((product) =>
+        Number(product.price || 0) * Number(product.quantity || 0)
+      )
+    ) || 0;
+
+    // gardes
+    if (!state.isAuth) {
+      return <Redirect to={SIGNIN} />;
+    }
+
+    if (!basketArray.length) {
+      return <Redirect to="/" />;
+    }
+
+    const finalSubtotal = Number(subtotal + shippingFee) || 0;
+
     return (
       <Component
-        // eslint-disable-next-line react/jsx-props-no-spreading
         {...props}
-        basket={state.basket}
+        basket={basketArray}
         payment={state.payment}
         profile={state.profile}
         shipping={state.shipping}
-        subtotal={Number(subtotal + shippingFee)}
+        subtotal={finalSubtotal}
       />
     );
-  }
-  return null;
-});
+  });
 
 export default withCheckout;
